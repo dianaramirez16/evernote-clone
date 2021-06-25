9b382d9457c07d86a17fdd7546e3f44985259667
Diana Ramirez <37516634+dianaramirez16@users.noreply.github.com>
2021-06-24 23:41:05 -0700

    app is updating input text to console

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import SidebarComponent from './sidebar/sidebar';
+import EditorComponent from './editor/editor';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state={
+      selectedNoteIndex: null,
+      selectedNote: null,
+      notes: null
+    };
+  }
+  render() {
+    return(
+      <div className="app-container">
+        <SidebarComponent></SidebarComponent>
+        <EditorComponent></EditorComponent>
+
+      </div>
+    );
+  }
+
+  componentDidMount = () => {
+    firebase
+      .firestore()
+      .collection('notes')
+      .onSnapshot(serverUpdate => {
+        const notes = serverUpdate.docs.map(_doc => {
+          const data = _doc.data();
+          data['id'] = _doc.id;
+          return data;
+        });
+        console.log(notes);
+        this.setState({notes:notes});
+      });
+  } 
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { firebase } from '@firebase/app'
+
+
+require('firebase/firestore');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBJ7NYERxorbd2U6RCTW8dAUY2jH67Z6Vw",
+  authDomain: "evernote-clone-ef943.firebaseapp.com",
+  projectId: "evernote-clone-ef943",
+  storageBucket: "evernote-clone-ef943.appspot.com",
+  messagingSenderId: "153203045223",
+  appId: "1:153203045223:web:3fbd1e48a78e7ecd7d35c2",
+  measurementId: "G-XZBRGB43T6"
+});
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  
+    <App />,
+  
+  document.getElementById('evernote-container')
 );
 
 // If you want to start measuring performance in your app, pass a function
