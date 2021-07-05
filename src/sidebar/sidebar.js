0ad1f169be837e1d60bb58a7eae09afff03eb811
Diana Ramirez <37516634+dianaramirez16@users.noreply.github.com>
2021-07-04 22:02:55 -0700

    adding item to sidebar

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -8,10 +8,54 @@ import SidebarItemComponent from '../sidebarItem/sidebarItem';
 class SidebarComponent extends React.Component{
     constructor() {
         super();
+        this.state = {
+            addingNote: false,
+            title: null,
+        };
     }
     render(){
-        return(<div>Hello from sidebar</div>)
+        const{ notes, classes, selectedNoteIndex} = this.props;
+
+        return(
+            <div className={classes.SidebarContainer}>
+                <Button
+                    onClick={this.newNoteBtnClick}
+                    className={classes.newNoteBtn}>
+                        {this.state.addingNote ? 'Cancel' : 'New Note'}
+                </Button>
+                {
+                    this.state.addingNote ? 
+                    <div>
+                        <input type="text" className={classes.newNoteInput}
+                        placeholder='Enter note title'
+                        onKeyUp={(e) => this.updateTitle(e.target.value)}>
+                        </input>
+                        <Button 
+                        className={classes.newNoteSubmitBtn}
+                        onClick={this.newNote}>
+                            Submit Note
+                        </Button>
+                    </div> :
+                    null
+                }
+            </div>
+        );
+    }
+    newNoteBtnClick = () => {
+        this.setState({
+            title: null,//makes note reset if closed 
+            addingNote: !this.state.addingNote 
+        });
+        console.log('newbtn clicked');
+    }
+    updateTitle = (txt) => {
+        this.setState({title: txt});
+        console.log('Here', txt);
+    }
+    newNote = () => {
+        console.log(this.state);
     }
 }
+
 
 export default withStyles(styles)(SidebarComponent);
